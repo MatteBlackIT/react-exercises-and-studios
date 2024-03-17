584376952561497bafb753c855344292742607a2
MatteBlackIT <bouwkamp.IT@gmail.com>
2024-03-17 03:39:37 -0500

    I get this more than most

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Formula 1</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>The average cost of a car is $7 million.</li>
+            <li>Brake discs can reach 1,000 degrees Celsius</li>
+            <li>Formula 1 cars can go upside down</li>
          </ol>
       </div>
    );
